3051579c779b83c84e1ca53e4af8487938a59211
Misaka-L <lipww1234@foxmail.com>
2025-02-08 01:03:14 +0800

    chore: remove dist

--- dist/cli.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-import{program as e}from"commander";e.name("yet-another-zhlint-cli").description("Yet another zhlint CLI").version(process.env.npm_package_version??"snapshot"),e.argument("<file-pattern>").option("--fix","fix all possibly found errors"),e.parse(),console.log("test");
